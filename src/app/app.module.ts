7189e114b622626416b7b23f90369b015ac01b05
pwakefield88 <paul.wakefield@soprasteria.com>
2019-11-04 12:51:02 +0000

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule }     from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
-  declarations: [ AppComponent, TopBarComponent, LoginComponent ],
+  declarations: [ AppComponent, TopBarComponent, LoginComponent, WelcomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
